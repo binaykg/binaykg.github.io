ba67e1a2712d8fe5f06e7e1054f1918658adec60
binaykg <giri.kumar.binay@gmail.com>
2026-07-08 15:36:29 +0530

    testing after changing script

--- add_entry.sh
+++ add_entry.sh
@@ -15,81 +15,71 @@ read -p "Link: " LINK
 
 # Convert title to filename
 FILENAME=$(echo "$TITLE" | tr '[:upper:]' '[:lower:]' | tr ' ' '-' | tr -cd '[:alnum:]-')
+
 POST_FILE="$POST_DIR/$FILENAME.html"
+
 
 # Create post page
 cat > "$POST_FILE" <<EOF
-
 <!DOCTYPE html>
 <html>
 <head>
-<title>Post</title>
+<title>$TITLE</title>
 
 <style>
 body {
-  margin: 0;
-  background: #fff;
+  font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+  max-width: 700px;
+  margin: 70px auto;
+  padding: 20px;
   color: #1d1d1f;
-  font-family: -apple-system, BlinkMacSystemFont, "Helvetica Neue", sans-serif;
-}
-
-.container {
-  max-width: 720px;
-  margin: 80px auto;
-  padding: 0 24px;
 }
 
 h1 {
-  font-size: 38px;
-  font-weight: 600;
-  letter-spacing: -0.8px;
-  margin-bottom: 35px;
+  font-size: 36px;
+  margin-bottom: 30px;
 }
 
-.content {
+p {
   font-size: 18px;
-  line-height: 1.7;
-  color: #333336;
-}
-
-.content p {
-  margin-bottom: 24px;
+  line-height: 1.6;
+  color: #333;
 }
 </style>
 
 </head>
+
 <body>
 
-<div class="container">
-<h1>$TITLE<</h1>
+<h1>$TITLE</h1>
 
-<div class="content">
-<p>$DESCRIPTION </p>
+<p>
+$DESCRIPTION
+</p>
+
 <a href="$LINK">$LINK</a>
-</div>
-</div>
 
 </body>
 </html>
-
 EOF
 
 
-# Add entry before closing ul tag in index.html
-ENTRY="
-  <li>
-    <a href=\"posts/$FILENAME.html\">
-      <strong>$TITLE</strong><br>
-      <span>$DESCRIPTION</span>
-    </a>
-  </li>
-"
+# Add entry to index.html before closing ul tag
+ENTRY="  <li><a href=\"posts/$FILENAME.html\"><strong>$TITLE</strong><br><span>$DESCRIPTION</span></a></li>"
 
-sed -i "/<\/ul>/i\\$ENTRY" index.html
+awk -v entry="$ENTRY" '
+/<\/ul>/ {
+    print entry
+}
+{
+    print
+}' index.html > index.tmp && mv index.tmp index.html
 
 
+echo ""
 echo "Created:"
 echo "$POST_FILE"
+
 echo "Updated index.html"
 
 
@@ -98,10 +88,9 @@ git add index.html "$POST_FILE"
 
 git commit -m "Add post: $TITLE"
 
-# Push changes
 git push
+
 
 echo ""
 echo "Done!"
-echo "Created: $POST_FILE"
 echo "Committed and pushed to Git."
